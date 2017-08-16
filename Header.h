ff4fa76927e8cdbf14090d378bd4e60afbfe4c50
Thatchapon Unprasert <poomst@hotmail.com>
2017-08-16 19:14:30 +0700

    iOS 7/8 support.

---
+++ Header.h
@@ -0,0 +1,22 @@
+#define UNRESTRICTED_AVAILABILITY
+#import "../PS.h"
+#import <UIKit/UIApplication+Private.h>
+
+@interface CAMFlipButton (ToBeFilterButton)
+- (void)setOn:(BOOL)on;
+- (UIImage *)_flipOnImage;
+@end
+
+@interface CAMFlipButton (CamSwitchDown)
+- (void)cms_updateImage:(BOOL)isVideo;
+- (UIImage *)_flipImage2;
+@end
+
+@interface CAMFilterButton (CamSwitchDown)
+- (void)cms_updateImage:(BOOL)isVideo;
+- (UIImage *)_filterImage2;
+@end
+
+#define isPhotoMode(mode) (mode == 0 || mode == 4)
+#define isVideoMode(mode) (mode == 1 || mode == 2 || mode == 3 || mode == 6)
+#define cameraBundle [NSBundle bundleForClass:NSClassFromString(@ "CAMShutterButton")]
--- Makefile
+++ Makefile
@@ -1,11 +1,9 @@
-DEBUG = 0
-PACKAGE_VERSION = 1.4
-TARGET = iphone:latest
+PACKAGE_VERSION = 1.4.1
+TARGET = iphone:clang:latest:7.0
 
 include $(THEOS)/makefiles/common.mk
 
 TWEAK_NAME = CamSwitchDown
-CamSwitchDown_FILES = Tweak.xm
-CamSwitchDown_PRIVATE_FRAMEWORKS = CameraUI
+CamSwitchDown_FILES = TweakCommon.xm Tweak9.xm Tweak8.xm Tweak7.xm
 
 include $(THEOS_MAKE_PATH)/tweak.mk
